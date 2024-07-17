cacb452dd3afec8127a0f7e304735444bb32c2b2
Jhone-cmd <jhone.1941@gmail.com>
2024-07-17 15:38:54 -0300

    chore: controller profile

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,16 +1,26 @@
-import { InvalidCredentialsError } from "@/errors/invalid-credentials-error";
+import { ResourceNotFoundError } from "@/errors/resource-not-found-error";
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
 
     try {
-    await request.jwtVerify();   
+        
+        const getUserProfileUseCase = makeGetUserProfileUseCase();
+        const { user } = await getUserProfileUseCase.execute({
+            userId: request.user.sub
+        });
             
-    return reply.status(200).send();
+        return reply.status(200).send({ 
+            user: {
+                ...user,
+                password_hash: undefined
+            }
+        });
 
     } catch (error) {     
-        if (error instanceof InvalidCredentialsError) return reply.status(400).send({ message: error.message });
+        if (error instanceof ResourceNotFoundError) return reply.status(404).send({ message: error.message });
 
         throw error;
     }
