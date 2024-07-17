d00ca99510622a6c44f6a422ce7fa6cc685abf33
Jhone-cmd <jhone.1941@gmail.com>
2024-07-17 17:48:54 -0300

    chore: prisma test environment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,32 @@
+import "dotenv/config";
+import { prisma } from "@/lib/prisma";
+import { execSync } from "node:child_process";
+import { randomUUID } from "node:crypto";
 import { Environment } from "vitest";
 
-export default <Environment>{
+function generateDatabaseUrl(schema: string) {
+    if (!process.env.DATABASE_URL) throw new Error('Please provide DATABASE_URL environment variable');
+
+    const url = new URL(process.env.DATABASE_URL);
+    url.searchParams.set('schema', schema);
+
+    return url.toString();
+}
+
+export default <Environment> {
     name: 'prisma',
     transformMode: 'ssr',
     async setup() {
-        console.log('setup')
-
+        const schema = randomUUID();
+        const databaseUrl =generateDatabaseUrl(schema);
+        process.env.DATABASE_URL = databaseUrl;
+        execSync('yarn prisma migrate deploy');
+        
         return {
-           async teardown() {},
+           async teardown() {
+                await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`);
+                await prisma.$disconnect();
+           },
         }
     },
 }
